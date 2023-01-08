c87321377824c4b914920370b76fe567456c97e2
ViSerge <viktoriya.andreeva1999@mail.ru>
2023-01-08 21:21:30 +0400

    add les 2 dz 1

---
+++ les2dz1/Program.cs
@@ -0,0 +1,9 @@
+﻿ //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+string num_st = Convert.ToString(num);
+Console.WriteLine(num_st[1]);
