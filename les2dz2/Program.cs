3b3ac493c5f0105f41ed1a67d4d0237af23cccb3
ViSerge <viktoriya.andreeva1999@mail.ru>
2023-01-08 21:32:11 +0400

    add les 2 dz 2

---
+++ les2dz2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+string num_st = Convert.ToString(num);
+if (num_st.Length < 3)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(num_st[2]);
+}
