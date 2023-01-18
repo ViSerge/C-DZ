a6716e3a43f8ec94dd53f91e3a1238b8409477d8
ViSerge <viktoriya.andreeva1999@mail.ru>
2023-01-19 00:05:00 +0400

    add les8dz3

---
+++ les8dz3/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+void CreateArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+
+void WriteArray (int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write(arr[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+Console.Write("Введите количество строк в массиве 1: ");
+int pos1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве 1 и строк в массиве 2: ");
+int pos2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве 2: ");
+int pos3 = Convert.ToInt32(Console.ReadLine());
+
+int[,] firstMartrix = new int[pos1, pos2];
+CreateArray(firstMartrix);
+Console.WriteLine($"Первая матрица:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[pos2, pos3];
+CreateArray(secomdMartrix);
+Console.WriteLine($"Вторая матрица:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[pos1,pos3];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+WriteArray(resultMatrix);
